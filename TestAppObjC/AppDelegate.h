df6fa78535667690e6f59158e82a01d8dd914d60
Jay Burgess <jragingfury@gmail.com>
2021-10-27 13:16:38 -0600

    sample project

---
+++ TestAppObjC/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TestAppObjC
+//
+//  Created by Jay Burgess on 10/27/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TestAppObjC/AppDelegate.m
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.m
+//  TestAppObjC
+//
+//  Created by Jay Burgess on 10/27/21.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    DDLogVerbose(@"foo");
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
